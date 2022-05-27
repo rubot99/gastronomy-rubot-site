715dd0b780fc9ac11aa8bd82ede358b3aecef21d
Reuben Raj <helix99@gmail.com>
2022-05-27 18:04:47 +0100

    Updated blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Gastronomy with Rubot`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`,
